944e0cf5bf93675a759dbd64270a263426174ef3
dzarpelon <zarpelon@outlook.com>
2022-04-01 14:01:11 +0200

    Corrected uninstall script and updated readme

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # homebrew install and remove scripts
 Just a couple of simple scripts to install or remove homebrew on MacOS. <br />
-The install script **do not** install the pre-reqs for homebrew yet. <br />
 Please read https://docs.brew.sh/Installation?msclkid=e79ba8ddb1aa11ecb222ca89027d8799 for all the reqs to install homebrew. <br />
 ***Uninstalling homebrew will REMOVE ALL PACKAGES INSTALLED BY IT!!!***
 
--- homebrew_uninstall.sh
+++ homebrew_uninstall.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 #test if brew is installed, if it is remove it. !!!! THIS WILL ALSO REMOVE ANY PACKAGES INSTALLED WITH BREW !!!!
-if !which brew > /dev/null; then
-    { echo "Brew is not installed, nothing to do"; }
-    
-else
-    { echo "Brew is installed, removing it" $'\n'; echo "!!!! THIS WILL ALSO REMOVE ANY PACKAGES INSTALLED WITH BREW !!!!";}
+if which brew > /dev/null; then
+       { echo "Brew is installed, removing it" $'\n'; echo "!!!! THIS WILL ALSO REMOVE ANY PACKAGES INSTALLED WITH BREW !!!!";}
     while true; do
     read -p "Do you wish to install this program? (Please use Y/N) " yn
     case $yn in
@@ -13,6 +10,10 @@ else
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
-done
+    done
+    
+else
+
+ { echo "Brew is not installed, nothing to do"; }
 
 fi
